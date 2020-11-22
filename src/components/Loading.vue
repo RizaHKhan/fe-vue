08e628ffa9911ae11c1d9b4eaaa3a7547b6dd17e
khanr <khanriza@gmail.com>
2020-11-22 00:40:36 -0500

    added most of authentication

---
+++ src/components/Loading.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="loading-container">
+    <div class="loader"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.loading-container {
+  position: absolute;
+  z-index: 9999;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  background-color: rgba(0,0,0, 0.5);
+
+  .loader {
+    margin: auto;
+    border: 16px solid #f3f3f3;
+    border-top: 16px solid #3bb40b;
+    border-radius: 50%;
+    width: 120px;
+    height: 120px;
+    animation: spin 2s linear infinite;
+  }
+
+  @keyframes spin {
+    0% { transform: rotate(0deg)}
+    100% { transform: rotate(360deg)}
+  }
+}
+</style>
---
+++ src/components/LoggedOut.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="loggedout">
+    <div class="intro">
+      <h1>Scrapper-Pro</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius alias doloremque esse! Voluptatibus iusto sapiente itaque. Cupiditate voluptas animi quasi?</p>
+    </div>
+    <form>
+      <div class="post-type">
+        <button @click.prevent="type = 'login'" class="type" :class="type === 'login' ? 'active' : ''">Login</button>
+        <button @click.prevent="type = 'register'" class="type" :class="type === 'register' ? 'active' : ''">Register</button>
+      </div>
+      <label for="username">Username:</label>
+      <input type="text" name="username" v-model="user.username">
+      <label for="password">Password:</label>
+      <input type="password" name="password" v-model="user.password">
+      <button @click.prevent="sendRequest(type)" class="submit">{{ type }}</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from '@/services/axios.js'
+import { mapMutations } from 'vuex'
+
+export default {
+  data: () => ({
+    type: 'login',
+    user: {
+      username: '',
+      password: ''
+    }
+  }),
+  methods: {
+    ...mapMutations({
+      updateLoading: "ui/updateLoading",
+      updateUserState: "user/updateUserState"
+    }),
+    async sendRequest(type) {
+      this.updateLoading(true)
+      try {
+        const response = await axios.post(`/api/${type}`, { username: this.user.username, password: this.user.password })
+        // set token to local storage
+        localStorage.setItem('scrapper-pro-token', response.data.token)
+        this.updateUserState(response.data)
+        this.updateLoading(false)
+      } catch (error) {
+        this.updateLoading(false)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.loggedout {
+  padding: 2em;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  @media (max-width: 779px) {
+    grid-template-columns: 1fr;
+  }
+}
+
+form {
+  display: grid;
+  grid-gap: 5px;
+
+  .post-type {
+    display: grid;
+    grid-template-columns: 100px 100px;
+    grid-gap: 5px;
+
+    .type {
+      border: 0;
+      padding: 0.5rem;
+    }
+
+    .type.active {
+      background: black;
+      color: white;
+    }
+  }
+
+  .submit {
+    text-transform: uppercase;
+    justify-self: end;
+    width: 100px;
+    border: 0;
+    padding: 0.5rem;
+    background: black;
+    color: white;
+  }
+}
+</style>
